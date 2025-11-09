963e4645e233bad52771e7a250ef4d9e4fe0a890
LKenji <54600289+lucaskmk@users.noreply.github.com>
2025-11-09 00:09:23 -0300

    fim porem sugestao estrnaha ex 2

--- exe2/main.c
+++ exe2/main.c
@@ -7,62 +7,83 @@ const int LED_PIN_B = 9;
 const int LED_PIN_Y = 5;
 const int BTN_PIN   = 28;
 
-struct repeating_timer timer_b; volatile bool ledb_state = false;
-struct repeating_timer timer_y; volatile bool ledy_state = false;
+struct repeating_timer timer_b; volatile int f_timer_b = 0;//flag do timer
+struct repeating_timer timer_y; volatile int f_timer_y = 0;//flag do timer
+volatile int flag_b = 0;//flag do botao
 
-volatile alarm_id_t alarm_cb_ = 0; volatile bool is_blinking = false;
+volatile int r_alarm_0 = 0;//alarm runinng
+ volatile int f_alarm_0 = 0;//flag do timer
 
-bool timer_callback_b(struct repeating_timer *t) {
-    ledb_state = !ledb_state;          // inverte estado
-    gpio_put(LED_PIN_B, ledb_state);    // atualiza LED
-    return true; // mantém o timer rodando
+int64_t alarm_0_callback(alarm_id_t id, void *user_data) {
+    f_alarm_0 = 1;r_alarm_0 = 0;
+    return 0;}
+
+bool timer_b_callback(struct repeating_timer *t) {
+    f_timer_b = 1;
+    return true;} // mantém o timer rodando
+bool timer_y_callback(struct repeating_timer *t) {
+    f_timer_y = 1;
+    return true;} //
+
+    void btn_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // fall edge
+        if (gpio == BTN_PIN) {
+            flag_b =1;}
+    } 
 }
-bool timer_callback_y(struct repeating_timer *t) {
-    ledy_state = !ledy_state;          // inverte estado
-    gpio_put(LED_PIN_Y, ledy_state);    // atualiza LED
-    return true; // mantém o timer rodando
-}
 
-// callback do alarme vermelho
-static int64_t alarm_cb_b(alarm_id_t id, void *user_data) {
-    is_blinking = false;
-    cancel_repeating_timer(&timer_b);gpio_put(LED_PIN_B, 0);
-    cancel_repeating_timer(&timer_y);gpio_put(LED_PIN_Y, 0);
-    return 0; // one-shot
-}
-
-void btn_callback(uint gpio, uint32_t events) {
-    if (gpio == BTN_PIN) {
-        if (events & GPIO_IRQ_EDGE_FALL) { // pressionado
-            
-            if (!is_blinking){ is_blinking = true;
-            alarm_cb_ = add_alarm_in_ms(5000, alarm_cb_b, NULL, false);//5000ms 5segundos
-            add_repeating_timer_ms(150, timer_callback_b, NULL, &timer_b); // T / 2? para 500ms 
-            add_repeating_timer_ms(500,timer_callback_y, NULL, &timer_y);}
-            
-        }
-        //if (events & GPIO_IRQ_EDGE_RISE) { // solto
-            //cancel_alarm(alarm_r); }
-        }
-    }
 int main() {
     stdio_init_all();
-
-    gpio_init(LED_PIN_Y);
-    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
-    gpio_put(LED_PIN_Y, 0);
-
-    gpio_init(LED_PIN_B);
+        gpio_init(LED_PIN_B);
     gpio_set_dir(LED_PIN_B, GPIO_OUT);
-    gpio_put(LED_PIN_B, 0);
-
+        gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
     gpio_init(BTN_PIN);
     gpio_set_dir(BTN_PIN, GPIO_IN);
     gpio_pull_up(BTN_PIN);
-    gpio_set_irq_enabled_with_callback(
-        BTN_PIN, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
-        
-    while (true) {
 
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true,
+                                       &btn_callback);
+
+
+    int r_timer_b = 0;//timer runinng
+    int r_timer_y = 0;//timer runinng
+    alarm_id_t alarm_0;//alarme globaaalll
+    int led_stateb = 0;
+    int led_statey = 0 ;
+    while (true) {
+            if (flag_b) {flag_b = 0;
+                if (!r_timer_b){r_timer_b = 1;//variavel para falar que esta rodando
+                    add_repeating_timer_ms(500, timer_b_callback, NULL,&timer_b);//500ms
+                }
+                if (!r_timer_y){r_timer_y= 1;//variavel para falar que esta rodando
+                    add_repeating_timer_ms(150, timer_y_callback, NULL,&timer_y);//500ms
+                }
+                if (!r_alarm_0){
+                r_alarm_0 = 1;
+                alarm_0 = add_alarm_in_ms(5000, alarm_0_callback, NULL, false);}
+             }
+        if (f_alarm_0){
+            f_alarm_0 = 0;
+            
+            r_timer_b = 0;
+            cancel_repeating_timer(&timer_b);
+            gpio_put(LED_PIN_B, 0);
+            r_timer_y = 0;
+            cancel_repeating_timer(&timer_y);
+            gpio_put(LED_PIN_Y, 0);
+        }
+        if(f_timer_b){// flag do timer ativado, desativa e faz oq tem q fazer
+            f_timer_b = 0;
+
+            led_stateb = !led_stateb;
+            gpio_put(LED_PIN_B, led_stateb);
+        }
+        if(f_timer_y){// flag do timer ativado, desativa e faz oq tem q fazer
+            f_timer_y = 0;
+
+            led_statey = !led_statey;
+            gpio_put(LED_PIN_Y, led_statey);
+        }
     }
-    }
+}
